0adf310eb6e513036e265bf60a89b18f09e24052
Abhirag Nagpure <abhiragnagpure@gmail.com>
2024-06-16 11:10:26 -0400

    Add line geometry from spline

--- demo-3/index.js
+++ demo-3/index.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
-
 import { OrbitControls } from "jsm/controls/OrbitControls.js";
+import spline from "./spline.js";
 
 const height = window.innerHeight;
 const width = window.innerWidth;
@@ -19,22 +19,19 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDampign = true;
 controls.dampingFactor = 0.03;
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshStandardMaterial({
-    color: 0xffff00
+const points = spline.getPoints(100);
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+const material = new THREE.LineBasicMaterial({
+    color: 0xff0000
 });
-
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const line = new THREE.Line(geometry, material);
+scene.add(line);
 
 const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
 scene.add(hemiLight);
 
 function animate(){
     requestAnimationFrame(animate);
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.02;
     renderer.render(scene, camera);
     controls.update();
 }
