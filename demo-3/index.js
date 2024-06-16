dac3b9f451db23f3c2613b2cb8f6df99242f472b
Abhirag Nagpure <abhiragnagpure@gmail.com>
2024-06-16 12:08:50 -0400

    Add tube geometry and camera movement

--- demo-3/index.js
+++ demo-3/index.js
@@ -6,6 +6,8 @@ const height = window.innerHeight;
 const width = window.innerWidth;
 
 const scene = new THREE.Scene();
+scene.fog = new THREE.FogExp2(0x000000, 0.2);
+
 const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
 camera.position.z = 5;
 
@@ -16,7 +18,7 @@ renderer.outputColorSpace = THREE.SRGBColorSpace;
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDampign = true;
+controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
 const points = spline.getPoints(100);
@@ -25,13 +27,34 @@ const material = new THREE.LineBasicMaterial({
     color: 0xff0000
 });
 const line = new THREE.Line(geometry, material);
-scene.add(line);
 
-const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
-scene.add(hemiLight);
+const tubeGeometry = new THREE.TubeGeometry(spline, 222, 0.65, 16, true);
+const tubeMaterial = new THREE.MeshBasicMaterial({
+    color: 0x0099ff,
+    side: THREE.DoubleSide,
+    wireframe: true
+});
 
-function animate(){
+const edges = new THREE.EdgesGeometry(tubeGeometry, 0.2);
+const lineMaterial = new THREE.LineBasicMaterial({
+    color: 0xffffff
+});
+const tubeLines = new THREE.LineSegments(edges, lineMaterial);
+scene.add(tubeLines);
+
+function updateCamera(t) {
+    const time = t * 0.5;
+    const looptime = 20*1000;
+    const p = (time % looptime) / looptime;
+    const pos = tubeGeometry.parameters.path.getPointAt(p);
+    const lookAt = tubeGeometry.parameters.path.getPointAt((p+0.01));
+    camera.position.copy(pos);
+    camera.lookAt(lookAt);
+}
+
+function animate(t = 0) {
     requestAnimationFrame(animate);
+    updateCamera(t);
     renderer.render(scene, camera);
     controls.update();
 }
